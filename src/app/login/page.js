03398ff90749b45301c416456390032a3805bd9d
techieshruti <shrutichandra721@gmail.com>
2026-09-23 23:03:25 +0530

    added login authentication

---
+++ src/app/login/page.js
@@ -0,0 +1,38 @@
+"use client"
+import { useState } from 'react';
+import api from "@/lib/axios";
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+       try {
+        const response = await api.post('auth/login', { username, password });
+        localStorage.setItem('accessToken', response.data.accessToken);
+        console.log('Login successful:', response.data);
+      } catch (error) {
+        console.log('Login failed:', error);
+      }
+    };
+
+  return (
+    <main>
+    <h1>Login</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    </main>
+  );
+};
+
+export default LoginPage
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://dummy.json.com',
+  baseURL: 'https://dummyjson.com',
 })
 
 export default api;
